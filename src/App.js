e1e2b27936f74398f79f0997848ccba6e0f7a228
WhyBusyy <ybg6152@naver.com>
2024-02-19 19:19:54 +0900

    성능개선 1 : 개발자도구 & lazy import

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { createContext, useEffect, useState } from "react";
+import { lazy, Suspense, createContext, useEffect, useState } from "react";
 import "./App.css";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import logo from "./ahnyu-logo.png";
 import data from "./components/data";
 import ProductList from "./components/ProductList";
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
-import Detail from "./pages/Detail";
 import axios from "axios";
-import Cart from "./pages/Cart";
 import { useQuery } from "react-query";
 // 리액트 쿼리 - 실시간으로 데이터를 불러와서 보여줘야하는 사이트를 제작할 때 유용(SNS, 주식 등)
+
+// 메인 로딩 속도 개선을 위한 방법 : lazy import 사용하기 >> 별도의 JS파일로 분리됨
+// import Cart from "./pages/Cart";
+// import Detail from "./pages/Detail";
+const Cart = lazy(() => import("./pages/Cart"));
+const Detail = lazy(() => import("./pages/Detail"));
 
 export let Context1 = createContext();
 // context api == state보관함
@@ -71,91 +75,94 @@ function App() {
         </Container>
       </Navbar>
 
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <div
-                className="main-bg"
-                style={{ backgroundImage: "url(" + logo + ")" }}
-              ></div>
-              <ProductList products={products} />
+      {/* lazy import 사용 시 컴포넌트 로딩 시간 증가로 로딩 중 보여줄 화면을 Suspense를 통해 적용 */}
+      <Suspense fallback={<div>로딩 중</div>}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <div
+                  className="main-bg"
+                  style={{ backgroundImage: "url(" + logo + ")" }}
+                ></div>
+                <ProductList products={products} />
 
-              {/* ajax 이용해서 데이터 받아오기 - axios 사용 */}
-              <button
-                onClick={() => {
-                  // 로딩중 ui 띄우기
-                  setClickNum(clickNum + 1);
-                  if (clickNum === 1) {
-                    console.log(clickNum);
-                    axios
-                      .get("https://codingapple1.github.io/shop/data2.json")
-                      .then((result) => {
-                        console.log(result.data);
-                        let copy = [...products, ...result.data];
-                        setProducts(copy);
-                        // 로딩중 ui 제거
-                      })
-                      .catch((err) => {
-                        console.error(err);
-                      });
-                  } else if (clickNum === 2) {
-                    console.log(clickNum);
-                    axios
-                      .get("https://codingapple1.github.io/shop/data3.json")
-                      .then((result) => {
-                        console.log(result.data);
-                        let copy = [...products, ...result.data];
-                        setProducts(copy);
-                        // 로딩중 ui 제거
-                      })
-                      .catch((err) => {
-                        console.error(err);
-                      });
-                  } else if (clickNum > 2) {
-                    console.log(clickNum);
-                    alert("더 이상 상품이 없어요,,");
-                  }
+                {/* ajax 이용해서 데이터 받아오기 - axios 사용 */}
+                <button
+                  onClick={() => {
+                    // 로딩중 ui 띄우기
+                    setClickNum(clickNum + 1);
+                    if (clickNum === 1) {
+                      console.log(clickNum);
+                      axios
+                        .get("https://codingapple1.github.io/shop/data2.json")
+                        .then((result) => {
+                          console.log(result.data);
+                          let copy = [...products, ...result.data];
+                          setProducts(copy);
+                          // 로딩중 ui 제거
+                        })
+                        .catch((err) => {
+                          console.error(err);
+                        });
+                    } else if (clickNum === 2) {
+                      console.log(clickNum);
+                      axios
+                        .get("https://codingapple1.github.io/shop/data3.json")
+                        .then((result) => {
+                          console.log(result.data);
+                          let copy = [...products, ...result.data];
+                          setProducts(copy);
+                          // 로딩중 ui 제거
+                        })
+                        .catch((err) => {
+                          console.error(err);
+                        });
+                    } else if (clickNum > 2) {
+                      console.log(clickNum);
+                      alert("더 이상 상품이 없어요,,");
+                    }
 
-                  // 한번에 여러번 get 요청해야할 때
-                  // Promise.all([axios.get('/url1'),axios.get('/url2')])
-                  // .then(()=>{ })
-                }}
-              >
-                버튼
-              </button>
-            </>
-          }
-        />
-        <Route
-          path="/detail/:id"
-          element={
-            <Context1.Provider value={{ stock, products }}>
-              <Detail products={products} />
-            </Context1.Provider>
-          }
-        />
+                    // 한번에 여러번 get 요청해야할 때
+                    // Promise.all([axios.get('/url1'),axios.get('/url2')])
+                    // .then(()=>{ })
+                  }}
+                >
+                  버튼
+                </button>
+              </>
+            }
+          />
+          <Route
+            path="/detail/:id"
+            element={
+              <Context1.Provider value={{ stock, products }}>
+                <Detail products={products} />
+              </Context1.Provider>
+            }
+          />
 
-        {/* nested routes 문법 */}
-        <Route path="/about" element={<About />}>
-          <Route path="member" element={<div>멤버임</div>} />
-          <Route path="location" element={<div>위치임</div>} />
-          {/* 부모컴포넌트와 자식 컴포넌트 모두 보여줌(Outlet 써야함) */}
-        </Route>
-        {/* <Route path="/detail/member" element={<About />} />
+          {/* nested routes 문법 */}
+          <Route path="/about" element={<About />}>
+            <Route path="member" element={<div>멤버임</div>} />
+            <Route path="location" element={<div>위치임</div>} />
+            {/* 부모컴포넌트와 자식 컴포넌트 모두 보여줌(Outlet 써야함) */}
+          </Route>
+          {/* <Route path="/detail/member" element={<About />} />
         <Route path="/detail/location" element={<About />} /> */}
 
-        <Route path="*" element={<div>없는 페이지예요!</div>} />
-        {/* 404페이지 / "*"은 "그외"를 의미 */}
+          <Route path="*" element={<div>없는 페이지예요!</div>} />
+          {/* 404페이지 / "*"은 "그외"를 의미 */}
 
-        <Route path="/event" element={<Event />}>
-          <Route path="one" element={<div>첫 주문시 양말 증정</div>} />
-          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
-        </Route>
+          <Route path="/event" element={<Event />}>
+            <Route path="one" element={<div>첫 주문시 양말 증정</div>} />
+            <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
+          </Route>
 
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
